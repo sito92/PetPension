f6e5f01cc0a9e1d110a59c3203c77fe1e17d59ba
sito92 <arturstelmach92@gmail.com>
2017-11-18 10:14:46 +0100

    test commit

--- PetPension/PetPension/Controllers/HomeController.cs
+++ PetPension/PetPension/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace PetPension.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your applicaasdfasdftion description page.";
 
             return View();
         }
