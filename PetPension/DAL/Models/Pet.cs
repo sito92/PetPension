5a5cce158a9b34dae873bab9ef1bafa3a27cdc4f
robert <robson081192@gmail.com>
2017-11-25 11:17:37 +0100

    Adding DAL
    First DB model

---
+++ PetPension/DAL/Models/Pet.cs
@@ -0,0 +1,9 @@
+﻿namespace DAL.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public int RoomType { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PetPension/DAL/Models/PetPensionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class PetPensionContext: DbContext
+    {
+        public PetPensionContext(): base("PetPension")
+        {
+            
+        }
+
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
