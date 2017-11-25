3054ec88829b162048999d444331e33ce23934a8
sito92 <arturstelmach92@gmail.com>
2017-11-25 10:36:20 +0100

    Database

---
+++ PetPension/Database/Models/Reservation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }      
+        public DateTime CreatedTime { get; set; }
+        public ReservationTime ReservationTime { get; set; }
+        public Pet Pet { get; set; }
+    }
+}
---
+++ PetPension/Database/Models/ReservationTime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.Models
+{
+    public class ReservationTime
+    {
+        public int Id { get; set; }
+        public DateTime From { get; set; }
+        public DateTime To { get; set; }
+        public Reservation Reservation { get; set; }
+    }
+}
